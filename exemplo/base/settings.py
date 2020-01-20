0135e1e8176d140cba828a95aed1bc260dc98953
61246880 <jorge.plautz@gmail.com>
2020-01-19 21:32:26 -0300

    Instalado lib Decouple e configuraçoes
     close #18

--- exemplo/base/settings.py
+++ exemplo/base/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from decouple import config
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -22,8 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+@sr30&(38$xveb427u(d8!h2n98)9@6l(q_a!2n1d7qed@t2*'
 
+# Dados para integração com Pagarme
+CHAVE_LINDART_API_PRIVADA = config('CHAVE_LINDART_API_PRIVADA')
+CHAVE_LINDART_CRIPTOGRAFIA_PUBLICA = config('CHAVE_LINDART_CRIPTOGRAFIA_PUBLICA')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
--- exemplo/pagamentos/tests/test_pagina_pagamento.py
+++ exemplo/pagamentos/tests/test_pagina_pagamento.py
@@ -11,3 +11,8 @@ def test_status_code(client):
 def test_pagarme_javascript(client):
     resp = client.get(reverse('pagamentos:produto'))
     assert_contains(resp, 'script src="//assets.pagar.me/checkout/1.1.0/checkout.js"')
+
+
+def test_encription_key_is_present(client, settings):
+    resp = client.get(reverse('pagamentos:produto'))
+    assert_contains(resp, settings.CHAVE_LINDART_CRIPTOGRAFIA_PUBLICA)
